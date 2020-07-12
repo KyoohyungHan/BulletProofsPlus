6f9cef26953bc5ea8c5512efa9015c995a70c0af
KyoohyungHan <satanigh89@gmail.com>
2020-07-12 21:45:10 +0900

    Add print in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use rand_core::OsRng;
 use curve25519_dalek::scalar::Scalar;
 use merlin::Transcript;
-use improvedbulletproof::PublicKey;
-use improvedbulletproof::range::{RangeProof, RangeProver, RangeVerifier};
+use bulletproofsplus::PublicKey;
+use bulletproofsplus::range::{RangeProof, RangeProver, RangeVerifier};
 
 fn main() {
     let n = 32;
     let m = 16;
     let pk = PublicKey::new(n * m);
     let mut prover = RangeProver::new();
+    println!("Commit Values");
     for _i in 0..m {
         prover.commit(&pk, 31u64, Scalar::random(&mut OsRng));
     }
     let mut prover_transcript = Transcript::new(b"RangeProof Test");
+    println!("Prove Range");
     let proof: RangeProof = RangeProof::prove(
         &mut prover_transcript,
         &pk,
@@ -22,6 +24,7 @@ fn main() {
     let mut verifier_transcript = Transcript::new(b"RangeProof Test");
     let mut verifier = RangeVerifier::new();
     verifier.allocate(&prover.commitment_vec);
+    println!("Verify Proof");
     let result = proof.verify(
         &mut verifier_transcript,
         &pk,
@@ -29,4 +32,5 @@ fn main() {
         &verifier,
     );
     assert_eq!(result, Ok(()));
+    println!("done");
 }
